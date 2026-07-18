984e1c0dc641005ef860a0bf68fa63cb95738cb0
skillgarage <vastaplus@gmail.com>
2026-07-18 10:14:26 +0200

    Create main.c

---
+++ 3_SSD1306_OLED_Displaying_Soil_Moisture_Values/main.c
@@ -0,0 +1,26 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "soil_sensor.h"
+#include "oled_display.h"
+
+static const char *TAG = "AUTO_IRRIGATION";
+
+#define SOIL_WET 2200
+
+static void soil_sensor_task(void *arg){
+    while(1){
+        int raw_value = soil_sensor_read_raw();
+        ESP_LOGI(TAG, "Soil raw = %d, Limit = %d", raw_value, SOIL_WET);
+        oled_display_show_values(raw_value, SOIL_WET);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void app_main(){
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    soil_sensor_init();
+    oled_display_init();
+
+    xTaskCreate(soil_sensor_task, "soil_sensor", 4096, NULL, 5, NULL);
+}
